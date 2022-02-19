52deb74ab9d109bcb6ff3955a41f439cfec87830
mbradley-github <76399993+mbradley-github@users.noreply.github.com>
2022-02-18 18:31:27 -0700

    Add support for stripe payments
    Add cors and stripe libraries
    Setup stripe /payment endpoint

---
+++ routes/stripe.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+const dotenv = require("dotenv");
+dotenv.config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY_TEST);
+
+router.post("/payment", (req, res) => {
+    stripe.charges.create(
+        {
+            source: req.body.tokenId,
+            amount: req.body.amount,
+            currency: "usd",
+        }, 
+        (stripeError, stripeResponse) => {
+            if (stripeError) {
+                res.status(500).json(stripeError);
+            } else {
+                res.status(200).json(stripeResponse);
+            }
+        }
+    );
+});
+
+module.exports = router;
